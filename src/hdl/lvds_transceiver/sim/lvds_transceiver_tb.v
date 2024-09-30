b18c50bd466e87d99f49e199aa33b421ef5f549d
Cyril Herzog <34273171+CyrilHerzog@users.noreply.github.com>
2024-09-30 21:26:58 +0200

    Add files via upload

--- src/hdl/lvds_transceiver/sim/lvds_transceiver_tb.v
+++ src/hdl/lvds_transceiver/sim/lvds_transceiver_tb.v
@@ -20,7 +20,8 @@ module lvds_transceiver_tb;
     reg sim_trx_b_tlp_wr, sim_trx_b_tlp_rd;
     reg[55:0] sim_trx_a_tlp_i;
     reg[33:0] sim_trx_b_tlp_i;
-    
+
+    reg sim_test;
 
     integer write_count_trx_a;
     integer read_count_trx_a;
@@ -73,6 +74,7 @@ module lvds_transceiver_tb;
             sim_trx_b_tlp_rd   = 1'b0;
             sim_trx_a_tlp_i    = 56'b0;
             sim_trx_b_tlp_i    = 34'b0;
+            sim_test           = 1'b0;
     #10000  sim_clk_120_arst_n = 1'b1;
             sim_clk_200_arst_n = 1'b1;
             sim_sys_arst_n     = 1'b1;
@@ -84,14 +86,17 @@ module lvds_transceiver_tb;
             sim_sys_arst_n     = 1'b1;
             sim_cal_start      = 1'b1;
 
-    //wait(dut_transceiver_physical_layer_top_trx_a.o_sys_cal_done || dut_transceiver_physical_layer_top_trx_a.o_sys_cal_fail)
-    // trx-a => check output's after tab init cal
-    //$display("o_trx_a_cal_done: %b, o_trx_a_cal_fail: %b, o_trx_a_mon_edge_tabs: %d, o_trx_a_mon_delay_tabs: %d",
-    //         dut_transceiver_physical_layer_top_trx_a.o_cal_done, dut_transceiver_physical_layer_top_trx_a.o_cal_fail,
-    //         dut_transceiver_physical_layer_top_trx_a.o_mon_edge_tabs, dut_transceiver_physical_layer_top_trx_a.o_mon_delay_tabs);
+    wait (dut_transceiver_a.o_status_connect && dut_transceiver_b.o_status_connect);
+    // start with control test - flags
+    //
+    @(posedge sim_tlp_clk) sim_test = 1'b1;
+    @(posedge sim_tlp_clk) sim_test = 1'b0;
 
+    #200000000
+    $display("o_trx_a_mon_edge_tabs: %d, o_trx_b_mon_edge_tabs: %d",
+            dut_transceiver_a.o_mon_edge_tabs, dut_transceiver_b.o_mon_edge_tabs);
 
-    #90000000 $finish;
+    $finish;
     end
 
 
@@ -106,7 +111,7 @@ module lvds_transceiver_tb;
             write_count_trx_a = write_count_trx_a + 1;
             #100
             $display("write data trx a: = %h", sim_trx_a_tlp_i);
-            if (write_count_trx_a >= 5) // Limit to 100 writes
+            if (write_count_trx_a >= 20)
                 disable write_process_trx_a;
         end
     end
@@ -122,7 +127,7 @@ module lvds_transceiver_tb;
             write_count_trx_b = write_count_trx_b + 1;
             #100
             $display("write data trx b: = %h", sim_trx_b_tlp_i);
-            if (write_count_trx_b >= 5) // Limit to 100 writes
+            if (write_count_trx_b >= 5)
                 disable write_process_trx_b;
         end
     end
@@ -136,7 +141,7 @@ module lvds_transceiver_tb;
             @(posedge sim_tlp_clk) sim_trx_b_tlp_rd = 1'b0;
             read_count_trx_b = read_count_trx_b + 1;
             #10000
-            if (read_count_trx_b >= 5) // Limit to 100 reads
+            if (read_count_trx_b >= 20)
                 disable read_process_trx_b;
         end
     end
@@ -150,7 +155,7 @@ module lvds_transceiver_tb;
             @(posedge sim_tlp_clk) sim_trx_a_tlp_rd = 1'b0;
             read_count_trx_a = read_count_trx_a + 1;
             #10000
-            if (read_count_trx_a >= 5) // Limit to 100 reads
+            if (read_count_trx_a >= 5)
                 disable read_process_trx_a;
         end
     end
@@ -189,12 +194,13 @@ module lvds_transceiver_tb;
 
     lvds_transceiver_top #(
         .SIMULATION_ENABLE      (1),
+        .CTRL_MON_ENABLE        (1),
         .IDELAYE_REF_FREQ       (300),
         //
         .CONNECTION_TYPE        (0),
         .TLP_TX_WIDTH           (56),
         .TLP_RX_WIDTH           (34),
-        .TLP_ID_WIDTH           (4),
+        .TLP_ID_WIDTH           (3),
         .TLP_BUFFER_TYPE        (1),
         .TLP_BUFFER_ADDR_WIDTH  (4),
         .CRC_POLY               (8'h07),             
@@ -224,9 +230,19 @@ module lvds_transceiver_tb;
         .o_phys_clk_600_n       (FMC_LA02_N),
         .o_phys_tx_p            (FMC_LA04_P),
         .o_phys_tx_n            (FMC_LA04_N),
+        //
+        // STATUS
+        .o_status_connect       (),
+        //
         // CONTROL & MONITOR
         .i_ctrl_mon_clk         (sim_tlp_clk),
         .i_ctrl_mon_arst_n      (sim_sys_arst_n),
+        // LINK
+        .i_ctrl_pls_crc_dllp    (1'b0), 
+        .i_ctrl_pls_crc_tlp     (sim_test), 
+        .i_ctrl_pls_status_ack  (1'b0), // acknowledge status
+        .o_mon_status_rply      (), // hold and reset by ack
+        // PHYSICAL
         .i_ctrl_tab_delay_wr    (1'b0),
         .i_ctrl_tab_delay       (5'b00000),
         .o_mon_edge_tabs        (),
@@ -236,12 +252,13 @@ module lvds_transceiver_tb;
 
     lvds_transceiver_top #(
         .SIMULATION_ENABLE      (1),
+        .CTRL_MON_ENABLE        (1),
         .IDELAYE_REF_FREQ       (300),
         //
         .CONNECTION_TYPE        (1),
         .TLP_TX_WIDTH           (34),
         .TLP_RX_WIDTH           (56),
-        .TLP_ID_WIDTH           (4),
+        .TLP_ID_WIDTH           (3),
         .TLP_BUFFER_TYPE        (1),
         .TLP_BUFFER_ADDR_WIDTH  (4),
         .CRC_POLY               (8'h07),             
@@ -271,31 +288,24 @@ module lvds_transceiver_tb;
         .o_phys_clk_600_n       (),
         .o_phys_tx_p            (FMC_LA08_P),
         .o_phys_tx_n            (FMC_LA08_N),
+        //
+        // STATUS
+        .o_status_connect       (),
+        //
         // CONTROL & MONITOR
         .i_ctrl_mon_clk         (sim_tlp_clk),
         .i_ctrl_mon_arst_n      (sim_sys_arst_n),
+        // LINK
+        .i_ctrl_pls_crc_dllp    (sim_test), // sim_test
+        .i_ctrl_pls_crc_tlp     (1'b0),
+        .i_ctrl_pls_status_ack  (1'b0),
+        .o_mon_status_rply      (),
+        // PHYSICAL
         .i_ctrl_tab_delay_wr    (1'b0),
         .i_ctrl_tab_delay       (5'b00000),
         .o_mon_edge_tabs        (),
         .o_mon_delay_tabs       ()
     );
 
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
- 
 
     endmodule
